5cd9af5260983fd4876e78d44e6ecfbea089feb1
Verena Sousa <verena.ads19@hotmail.com>
2022-11-29 10:14:41 -0300

    Aula Grouping operator

--- EXPRESSIONS-OPERATORS/expressions_operators.js
+++ EXPRESSIONS-OPERATORS/expressions_operators.js
@@ -46,3 +46,46 @@ delete person.age
 console.log(person);
 */
 
+/*
+    Operadores Aritméticos
+
+    #Multiplicação *
+    console.log(5*5)
+
+    #Divisão / 
+    console.log(25/5)
+
+    #Soma +
+    console.log(34+67)
+
+    #Subtração - 
+    console.log(34-25)
+
+    #Resto da divisão
+    let remainder 
+    remainder = 11 % 3
+    console.log(remainder)
+
+    #Incremento ++
+    let increment = 0
+    increment++
+    console.log(increment)
+
+    #Decremento
+    let decrement = 5
+    decrement--
+    console.log(decrement)
+
+    #Exponencial
+    console.log(3 ** 3)
+
+*/
+
+/* Grouping operator
+
+let total = 2 + 3 * 5
+console.log(total)
+
+let total2 =  (2 + 3) * 5 
+console.log(total2);
+*/
