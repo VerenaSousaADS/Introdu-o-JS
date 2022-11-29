00525da4f70a534acd18f47acef9b7da34322e92
Verena Sousa <verena.ads19@hotmail.com>
2022-11-29 10:26:42 -0300

    Operadores de comparação igual, diferente, estritamente igual, estritamente diferente

---
+++ EXPRESSIONS-OPERATORS/script.js
@@ -0,0 +1,31 @@
+// Operadores de comparação 
+
+// Irá comparar valores e retornar um Boolean como resposta à comparação
+
+let one = 1
+let two = 2
+
+// == igual a
+
+console.log(two == 1)
+console.log(one == "1")
+
+
+// != diferente de
+
+console.log(one != two)
+console.log( one != 1)
+console.log(one != "1")
+
+// Comparação de valor e tipo
+
+// === estritamente igual a
+
+console.log(one === "1")
+console.log(one === 1)
+
+// !== estritamente diferente de 
+
+console.log(one !== "1")
+console.log(one !== 1)
+console.log(one !== two);
