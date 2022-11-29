49cab7fd818d953454a533d61d49c66d49e19f45
Verena Sousa <verena.ads19@hotmail.com>
2022-11-29 11:25:16 -0300

    Operadores Lógicos

--- EXPRESSIONS-OPERATORS/expressions_operators.js
+++ EXPRESSIONS-OPERATORS/expressions_operators.js
@@ -168,4 +168,23 @@ x %= 2
 
 console.log(x)
 
+// Operadores lógicos (logiacal operators)
+
+// 2 valores booleanos, quando verificados, resultará em verdadeiro ou falso
+
+let pao = true
+let queijo = false
+
+// AND &&
+
+console.log(pao && queijo)
+
+// OR ||
+
+console.log(pao || queijo)
+
+// NOT !
+console.log(!queijo)
+
+
 */
