4bec3604c472a1b32d54152c1c7b2b9f1c6c1e07
Verena Sousa <verena.ads19@hotmail.com>
2022-11-30 11:52:26 -0300

    Aula Switch

--- CONDITIONAL-FLOW/script.js
+++ CONDITIONAL-FLOW/script.js
@@ -1,5 +1,5 @@
 /// IF ... ELSE
-
+/*
 let temperature = 36.3
 let highTemperature = temperature >= 37.5
 let mediumTemperature = temperature < 37.5 && temperature >= 37
@@ -12,3 +12,51 @@ if(highTemperature){
 }else{
     console.log("Saudável")
 }
+
+*/
+
+
+let expression = ''
+
+switch(expression){// puxa a expressão para o switch
+    case 'a': // confere se o valor da expressão é o correto
+        console.log('a')
+        break // para a execução do switch apenas se verdadeiro
+    case 'b':
+        console.log('b')
+        break
+    default: // caso nenhum valor seja correto, realizará a instrução dentro de si
+        console.log('default')
+        break
+}
+
+// Calculadora
+
+function calculate(number1, operator, number2) {
+
+    let result = 0;
+    
+    switch (operator) {
+        case '+':
+            result = number1 + number2
+            break;
+        case '-':
+            result = number1 - number2
+            break;
+        case '*':
+            result = number1 * number2
+            break;
+        case '/':
+            result = number1 / number2
+            break;
+    
+        default:
+            console.log('não implementado')
+            break;
+    }
+
+    return result;
+}
+
+console.log(calculate(4, '%', 8))
+console.log(calculate(4, '*', 8))
