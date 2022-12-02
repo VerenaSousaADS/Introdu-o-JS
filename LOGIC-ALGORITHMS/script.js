2e0979e06131d73b3ac50e8fcf947340872999d0
Verena Sousa <verena.ads19@hotmail.com>
2022-12-02 13:59:35 -0300

    Continuando as aulas

---
+++ LOGIC-ALGORITHMS/script.js
@@ -0,0 +1,12 @@
+/*
+
+    Encontre a solução a o problema:
+    
+    Pergunte o nome do usuário e escreva a mensagem:
+    "Olá, [nome do usuário]"
+
+*/
+
+let nome = prompt("Qual seu nome?")
+
+alert(`Olá, ${nome}!`)
