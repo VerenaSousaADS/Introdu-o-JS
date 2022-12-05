76efaf8a46abe60c6e7740c73c5cac7e6136d445
Verena Sousa <verena.ads19@hotmail.com>
2022-12-05 11:33:59 -0300

    Aula de operandos

---
+++ LOGIC-ALGORITHMS/code.js
@@ -0,0 +1,56 @@
+/*
+
+    Encontre a solução a o problema:
+    
+    Pergunte o nome do usuário e escreva a mensagem:
+    "Olá, [nome do usuário]"
+
+
+
+let nome = prompt("Qual seu nome?")
+
+alert(`Olá, ${nome}!`)*/
+
+/* 
+  Solicite 2 números, faça a soma deles
+  e apresente o resultado final ao usuário
+
+
+alert("Vamos somar dois números")
+
+let numberOne = prompt("Digite o primeiro número: ")
+let numberTwo = prompt("Digite o segundo número: ")
+let result = Number(numberOne) + Number(numberTwo)
+
+alert("A soma destes dois números é: " + result)
+*/
+
+
+/*
+  Capturar 2 números
+  e fazer as operações matemáticas
+  de soma, subtração, multiplicação,
+  divisão e resto da divisão.
+
+  E para cada operação, mostrar um alerta
+  com o resultado.
+
+
+
+let firstNumber = prompt ("Digite o primeiro número")
+let secondNumber = prompt("Digite o segundo número")
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+const sum = firstNumber + secondNumber
+const sub = firstNumber - secondNumber
+const mult = firstNumber * secondNumber
+const div = firstNumber / secondNumber
+const restDiv = firstNumber % secondNumber
+
+alert("A soma é " + sum)
+alert("A subtração é " + sub)
+alert("A multiplicação é " + mult)
+alert("A divisão é " + div)
+alert("O resto da divisão é " + restDiv)*/
