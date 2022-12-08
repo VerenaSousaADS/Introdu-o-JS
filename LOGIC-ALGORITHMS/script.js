41465db18d8c7858c2aa3eb8603cd2cbfcfb1168
Verena Sousa <verena.ads19@hotmail.com>
2022-12-08 10:52:32 -0300

    Aula - Interagindo com um menu de opções - parte1

--- LOGIC-ALGORITHMS/script.js
+++ LOGIC-ALGORITHMS/script.js
@@ -1,39 +1,26 @@
-/*
-  ** Jogo da advinhação **
+let option
+let items = []
 
-  Apresente a mensagem ao usuário:
-  "Advinhe o número que estou pensando? Está entre 0 e 10"
-
-  Crie um lógica para gerar um número aleatório 
-  e verificar se o número digitado é o mesmo que o aleatório gerado pelo sistema.
-
-  Enquanto o usuário não adivinhar o número, mostrar a mensagem:
-  "Erro, tente novamente:"
-
-  Caso o usuário acerte o número, apresentar a mensagem:
-  "Parabéns! Você advinhou o número em x tentativas"
-
-  Substitua o "x" da mensagem, pelo número de tentativas
-
-
-let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10")
-const randomNumber = Math.round(Math.random() * 10)
-
-let xAttempts = 1
-
-while(Number(result) != randomNumber) {
-  result = prompt("Erro, tente novamente:")
-  xAttempts++
-}
-
-
-if(xAttempts != 1){
-    alert(`Parabéns! O número que eu pensei foi ${randomNumber} e você advinhou o número em ${xAttempts} tentativas`)
-
-}else{
-    alert(`Parabéns! O número que eu pensei foi ${randomNumber} e você advinhou o número em ${xAttempts} tentativa`)
+while(option != 3){
+  option = Number(prompt(` Olá usuário! Digite o número da opção desejada
     
-}
+  1. Cadastrar um item na lista
+  2. Mostrar itens cadastrados
+  3. Sair do programa`))
 
-console.log(randomNumber, xAttempts);
-*/
+  if(option == 1){
+    let item = prompt("Digite o nome do item")
+    items.push(item)
+  }else if(option == 2){
+    if (items.length == 0){
+      alert("Não existem itens cadastrados")
+    }else{
+      alert(items)
+    }
+
+  }else{
+    alert("Bye, Bye!")
+  }
+
+  console.log(option, items)
+};
